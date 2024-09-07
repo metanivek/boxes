f80ed48cd56671972f20604a177115d8bd287ea6
Kevin Smith <metanivek@gmail.com>
2024-09-07 17:45:11 -0400

    Add alacritty

---
+++ home/alacritty.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+{
+  home.packages = with pkgs; [ alacritty-theme ];
+
+  programs.alacritty = {
+    enable = true;
+
+    settings = {
+      import = [ "${pkgs.alacritty-theme}/catppuccin_mocha.toml" ];
+
+      working_directory = config.home.homeDirectory;
+
+      env = {
+        TERM = "xterm-256color";
+      };
+
+      font = {
+        size = 16;
+        normal = {
+          family = "FiraCode Nerd Font Mono";
+        };
+      };
+
+      selection = {
+        save_to_clipboard = true;
+      };
+
+      window = {
+        startup_mode = "Maximized";
+        decorations = "None";
+
+        position = {
+          x = 0;
+          y = 0;
+        };
+
+        padding = {
+          x = 0;
+          y = 0;
+        };
+      };
+    };
+  };
+}
--- home/default.nix
+++ home/default.nix
@@ -2,6 +2,7 @@
 {
   home-manager.users.${username} = {
     imports = [
+      ./alacritty.nix
       ./aliases.nix
       ./fzf.nix
       ./git.nix
