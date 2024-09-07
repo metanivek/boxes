e1f21a379af1968aed8a3bfaaff50c5a02d1342a
Kevin Smith <metanivek@gmail.com>
2024-09-07 18:46:47 -0400

    Organizing!

--- home/default.nix
+++ home/default.nix
@@ -2,22 +2,9 @@
 {
   home-manager.users.${username} = {
     imports = [
-      ./alacritty.nix
       ./aliases.nix
-      ./bat.nix
-      ./direnv.nix
-      ./eza.nix
-      ./fzf.nix
-      ./git.nix
-      ./gpg.nix
-      ./mise.nix
-      ./ripgrep.nix
       ./sessionVariables.nix
-      ./starship.nix
-      ./ssh.nix
-      ./tmux.nix
-      ./zoxide.nix
-      ./zsh.nix
+      ./programs
     ];
 
     home.stateVersion = "24.05";
---
+++ home/programs/default.nix
@@ -0,0 +1,19 @@
+{ ... }:
+{
+  imports = [
+    ./alacritty.nix
+    ./bat.nix
+    ./direnv.nix
+    ./eza.nix
+    ./fzf.nix
+    ./git.nix
+    ./gpg.nix
+    ./mise.nix
+    ./ripgrep.nix
+    ./starship.nix
+    ./ssh.nix
+    ./tmux.nix
+    ./zoxide.nix
+    ./zsh.nix
+  ];
+}
