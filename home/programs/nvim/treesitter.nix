bdb59098384bb1ad21a6991bb0e8769d3a285a92
Kevin Smith <metanivek@gmail.com>
2024-11-23 07:43:37 -0500

    Fully setup treesitter

--- home/programs/nvim/treesitter.nix
+++ home/programs/nvim/treesitter.nix
@@ -2,6 +2,80 @@
   programs.nixvim = {
     plugins.treesitter = {
       enable = true;
+      settings = {
+        auto_install = true;
+        ensure_installed = [
+          "bash"
+          "c"
+          "clojure"
+          "cpp"
+          "css"
+          "csv"
+          "diff"
+          "dockerfile"
+          "dot"
+          "elixir"
+          "elm"
+          "erlang"
+          "fennel"
+          "fsh"
+          "git_config"
+          "git_rebase"
+          "gitattributes"
+          "gitcommit"
+          "gitignore"
+          "gleam"
+          "go"
+          "gomod"
+          "graphql"
+          "haskell"
+          "hcl"
+          "helm"
+          "html"
+          "hurl"
+          "javascript"
+          "json"
+          "json5"
+          "just"
+          "kotlin"
+          "lua"
+          "luadoc"
+          "make"
+          "markdown"
+          "nginx"
+          "nix"
+          "ocaml"
+          "ocaml_interface"
+          "python"
+          "rust"
+          "sql"
+          "swift"
+          "terraform"
+          "toml"
+          "typescript"
+          "vue"
+          "xml"
+          "yaml"
+          "zig"
+        ];
+        highlight = {
+          enable = true;
+        };
+        incremental_selection = {
+          enable = true;
+
+          keymaps = {
+            init_selection = "gnn";
+            node_decremental = "grm";
+            node_incremental = "grn";
+            scope_incremental = "grc";
+          };
+        };
+
+        indent = {
+          enable = true;
+        };
+      };
     };
     plugins.treesitter-context = {
       enable = true;
