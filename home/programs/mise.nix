fe72473ee2199167f37699e4557414823dcbb836
Kevin Smith <metanivek@gmail.com>
2025-07-11 15:04:10 -0400

    add alpha's lfs jj branch

--- home/programs/mise.nix
+++ home/programs/mise.nix
@@ -6,6 +6,12 @@
         node = "20";
         python = "latest";
         go = "latest";
+        rust = "1.88.0";
+        "cargo:https://github.com/kejadlen/jj" = {
+          bin = "jj";
+          version = "branch:lfs";
+          crate = "jj-cli";
+        };
       };
     };
   };
